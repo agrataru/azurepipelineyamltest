874a41088f4488bb5c914414b3dfb728ff3bc067
agrataru <68077772+agrataru@users.noreply.github.com>
2021-02-08 18:43:25 +0530

    Update ymlparser.ps1

--- .azure-pipeline/ymlparser.ps1
+++ .azure-pipeline/ymlparser.ps1
@@ -12,12 +12,13 @@ $customerNameVal = $yaml.testconfig.customerName
 $accountNameVal = $yaml.testconfig.accountName
 $locationVal = $yaml.testconfig.location
 $cloudVal = $yaml.testconfig.cloud
+$cloudVal = $cloudVal.Trim()
 $emailVal = $yaml.testconfig.email
 $userNameVal = $yaml.testconfig.userName
 Write-Host '##vso[task.setvariable variable=accountType;isOutput=true]'$accountTypeVal.Trim()
 Write-Host '##vso[task.setvariable variable=customerName;isOutput=true]'$customerNameVal.Trim()
 Write-Host '##vso[task.setvariable variable=accountName;isOutput=true]'$accountNameVal.Trim()
 Write-Host '##vso[task.setvariable variable=location;isOutput=true]'$locationVal.Trim()
-Write-Host '##vso[task.setvariable variable=cloud;isOutput=true]'$cloudVal.Trim()
+Write-Host '##vso[task.setvariable variable=cloud;isOutput=true]'$cloudVal
 Write-Host '##vso[task.setvariable variable=email;isOutput=true]'$emailVal.Trim()
 Write-Host '##vso[task.setvariable variable=userName;isOutput=true]'$userNameVal.Trim()
